85453710e5dee7529b208f2404e3d3df62245d2a
UndeadTeam <contato@undeadbrasil.com>
2016-07-22 19:54:41 -0300

    FIX smartphone

--- mpmissions/Altis_Life.Altis/dialog/smartphone.hpp
+++ mpmissions/Altis_Life.Altis/dialog/smartphone.hpp
@@ -41,7 +41,198 @@ class controlsBackground {
 		h = 1.5;
 		colorBackground[] = {0, 0, 0, 0};
 	};
-	class Life_RscPicture_1203: Life_RscPicture{idc = 1203;text = "icons\Backpack.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1204: Life_RscPicture{idc = 1204;text = "icons\Banking.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1205: Life_RscPicture{idc = 1205;text = "icons\Actions.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1206: Life_RscPicture{idc = 1206;text = "icons\Crafting.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Crafting_Overlay: Life_RscPicture{idc = 12061;text = "icons\Crafting_Overlay.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1207: Life_RscPicture{idc = 1207;text = "icons\Barriers.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Barriers_Overlay: Life_RscPicture{idc = 12071;text = "icons\Barriers_Overlay.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1208: Life_RscPicture{idc = 1208;text = "icons\Keys.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1209: Life_RscPicture{idc = 1209;text = "icons\Licenses.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1210: Life_RscPicture{idc = 1210;text = "icons\Market.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Market_Overlay: Life_RscPicture{idc = 12101;text = "icons\Market_Overlay.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1211: Life_RscPicture{idc = 1211;text = "icons\Messages.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1212: Life_RscPicture{idc = 1212;text = "icons\Settings.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1213: Life_RscPicture{idc = 1213;text = "icons\Skills.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Skills_Overlay: Life_RscPicture{idc = 12131;text = "icons\Skills_Overlay.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1214: Life_RscPicture{idc = 1214;text = "icons\Sync_Data.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1215: Life_RscPicture{idc = 1215;text = "icons\Taxi.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Taxi_Overlay: Life_RscPicture{idc = 12151;text = "icons\Taxi_Overlay.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1216: Life_RscPicture{idc = 1216;text = "icons\Terror.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Terror_Overlay: Life_RscPicture{idc = 12161;text = "icons\Terror_Overlay.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Life_RscPicture_1217: Life_RscPicture{idc = 1217;text = "icons\Wanted.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class RscPicture_1202: Life_RscPicture{idc = 1218;text = "icons\Admin.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class Admin_Overlay: Life_RscPicture{idc = 12181;text = "icons\AdminRequestOverlay.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class RscPicture_1203: Life_RscPicture{idc = 1219;text = "icons\CallBackup.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};class CallBackup_Overlay: Life_RscPicture{idc = 12191;text = "icons\CallBackupOverlay.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};};
+	class Life_RscPicture_1203: Life_RscPicture{
+		idc = 1203;
+		text = "icons\Backpack.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1204: Life_RscPicture{
+		idc = 1204;
+		text = "icons\Banking.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1205: Life_RscPicture{
+		idc = 1205;
+		text = "icons\Actions.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1206: Life_RscPicture{
+		idc = 1206;
+		text = "icons\Crafting.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Crafting_Overlay: Life_RscPicture{
+		idc = 12061;
+		text = "icons\Crafting_Overlay.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1207: Life_RscPicture{
+		idc = 1207
+		text = "icons\Barriers.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Barriers_Overlay: Life_RscPicture{
+		idc = 12071;
+		text = "icons\Barriers_Overlay.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1208: Life_RscPicture{
+		idc = 1208;
+		text = "icons\Keys.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1209: Life_RscPicture{
+		idc = 1209;
+		text = "icons\Licenses.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1210: Life_RscPicture{
+		idc = 1210;
+		text = "icons\Market.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Market_Overlay: Life_RscPicture{
+		idc = 12101;
+		text = "icons\Market_Overlay.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1211: Life_RscPicture{
+		idc = 1211;
+		text = "icons\Messages.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1212: Life_RscPicture{
+		idc = 1212;
+		text = "icons\Settings.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1213: Life_RscPicture{
+		idc = 1213;
+		text = "icons\Skills.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Skills_Overlay: Life_RscPicture{
+		idc = 12131;
+		text = "icons\Skills_Overlay.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1214: Life_RscPicture{
+		idc = 1214;
+		text = "icons\Sync_Data.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1215: Life_RscPicture{
+		idc = 1215;
+		text = "icons\Taxi.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Taxi_Overlay: Life_RscPicture{
+		idc = 12151;
+		text = "icons\Taxi_Overlay.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1216: Life_RscPicture{
+		idc = 1216;
+		text = "icons\Terror.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Terror_Overlay: Life_RscPicture{
+		idc = 12161;
+		text = "icons\Terror_Overlay.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class Life_RscPicture_1217: Life_RscPicture{
+		idc = 1217;
+		text = "icons\Wanted.paa";
+		x = -0.0625;
+		y = -0.3;
+		w = 1.1375;
+		h = 1.5;
+		colorBackground[] = {0, 0, 0, 0};
+	};
+	class RscPicture_1202: Life_RscPicture{idc = 1218;text = "icons\Admin.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};
+	class Admin_Overlay: Life_RscPicture{idc = 12181;text = "icons\AdminRequestOverlay.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};
+	class RscPicture_1203: Life_RscPicture{idc = 1219;text = "icons\CallBackup.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};
+	class CallBackup_Overlay: Life_RscPicture{idc = 12191;text = "icons\CallBackupOverlay.paa";x = -0.0625;y = -0.3;w = 1.1375;h = 1.5;colorBackground[] = {0, 0, 0, 0};};};
 	
 	class controls {
 		class Life_RscButtonInvisible_2400: Life_RscButtonInvisible{
